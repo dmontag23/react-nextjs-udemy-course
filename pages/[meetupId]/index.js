f05c39c6e4a681d7fe94e5f3f31273c8b5d1bee3
Dillon Montag <dillon.montag@divido.com>
2021-08-15 16:55:47 +0100

    fix fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -30,7 +30,7 @@ export async function getStaticPaths() {
   await client.close();
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map(meetup => ({
       params: {
         meetupId: meetup._id.toString()
